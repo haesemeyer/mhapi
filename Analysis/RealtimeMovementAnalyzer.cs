3a09011867b43a0e0d8bc7ae8575e29b235f8d2f
Martin Haesemeyer <m.haesemeyer@gmail.com>
2013-02-28 14:15:43 -0500

    Added class to do point-by-point realtime analysis of fish movement

---
+++ Analysis/RealtimeMovementAnalyzer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MHApi.Analysis
+{
+
+    /// <summary>
+    /// Class to smoothen tracks, compute instant speeds
+    /// and detect bouts in realtime.
+    /// </summary>
+    public unsafe class RealtimeMovementAnalyzer : IDisposable
+    {
+    }
+}
