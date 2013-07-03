5f2082af1be3106e732bacdb8fee87fe1772c2c2
Martin Haesemeyer <m.haesemeyer@gmail.com>
2013-07-03 10:35:07 -0400

    Implemented effector representing a pulse-width modulated heater and an externally updated dummy temperature probe.

---
+++ PIDController/DummyTempSensor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MHApi.PIDController
+{
+    /// <summary>
+    /// Implementation of a temperature probe
+    /// repressentation with external update of
+    /// the temperature
+    /// </summary>
+    public class DummyTempSensor : Sensor
+    {
+        public double Temperature
+        {
+            get
+            {
+                return Value;
+            }
+            set
+            {
+                Value = value;
+            }
+        }
+    }
+}
