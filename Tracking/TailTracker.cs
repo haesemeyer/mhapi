4982eee570034701b9c6e4c8fcf4c78ff0d7205c
Martin Haesemeyer <m.haesemeyer@gmail.com>
2013-09-23 18:41:22 -0400

    Added method to create 2D array of points to sweep and added point sweep to tracking procedure in order to identify most likely tail angle.

--- Tracking/TailTracker.cs
+++ Tracking/TailTracker.cs
@@ -16,7 +16,11 @@ namespace MHApi.Tracking
     /// </summary>
     public unsafe class TailTracker : IDisposable
     {
-
+        /// <summary>
+        /// Represents the tracking result
+        /// for one tail segment giving its angle and distance
+        /// from the tail-start
+        /// </summary>
         public struct TailPoint
         {
             /// <summary>
@@ -30,6 +34,7 @@ namespace MHApi.Tracking
             /// </summary>
             public double Distance;
 
+            
             /// <summary>
             /// Constructs a new tailpoit
             /// </summary>
@@ -39,6 +44,35 @@ namespace MHApi.Tracking
             {
                 Angle = angle;
                 Distance = distance;
+            }
+        }
+
+        /// <summary>
+        /// Represents a point that should be testesd for the
+        /// presence of the tail during tracking
+        /// </summary>
+        private struct TailTestCoordinate
+        {
+            /// <summary>
+            /// The angle of this coordinate
+            /// </summary>
+            public double Angle;
+
+            /// <summary>
+            /// The radius of this coordinate
+            /// </summary>
+            public double Radius;
+
+            /// <summary>
+            /// The coordinate itself
+            /// </summary>
+            public IppiPoint Coordinate;
+
+            public TailTestCoordinate(double angle, double radius, IppiPoint coordinate)
+            {
+                Angle = angle;
+                Radius = radius;
+                Coordinate = coordinate;
             }
         }
 
@@ -124,6 +158,29 @@ namespace MHApi.Tracking
         /// </summary>
         uint _frameNumber;
 
+        /// <summary>
+        /// This 2D array contains all the points to scan
+        /// for each tail segment [NAngles,_nSegments]
+        /// </summary>
+        TailTestCoordinate[,] _pointsToScan;
+
+        object _scanPointLock = new object();
+
+        /// <summary>
+        /// The tail angle at which we start to scan (-90 degrees)
+        /// </summary>
+        const double ScanStartAngle = -1 * Math.PI / 2;
+
+        /// <summary>
+        /// We scan over a whole 180 degrees
+        /// </summary>
+        const double SweepAngle = Math.PI;
+
+        /// <summary>
+        /// The number of angle steps we scan over (901 means 0.2 degrees per step including the outer angles of -90 and +90)
+        /// </summary>
+        const int NAngles = 901;
+
         #endregion
 
         #region Constructor
@@ -156,7 +213,8 @@ namespace MHApi.Tracking
             //set up our track regions based on the tail positions
             DefineTrackRegions();
             NSegments = nsegments;
-            InitializeImageBuffers();
+            InitializeImageBuffers();//set up image buffers
+            InitializeScanPoints();//create scan points appropriate for the tail parameters
         }
 
         #endregion
@@ -220,6 +278,8 @@ namespace MHApi.Tracking
                 if (value < 1)
                     throw new ArgumentOutOfRangeException("NSegments");
                 _nSegments = value;
+                //redefine scan points
+                InitializeScanPoints();
             }
         }
 
@@ -240,6 +300,8 @@ namespace MHApi.Tracking
                 _tailStart = value;
                 //redefine track region
                 DefineTrackRegions();
+                //redefine scan points
+                InitializeScanPoints();
             }
         }
 
@@ -260,6 +322,8 @@ namespace MHApi.Tracking
                 _tailEnd = value;
                 //redefine track regions
                 DefineTrackRegions();
+                //redefine scan points
+                InitializeScanPoints();
             }
         }
 
@@ -334,6 +398,51 @@ namespace MHApi.Tracking
         }
 
         /// <summary>
+        /// Based on the current tail-start and tail-end
+        /// as well as the number of segments computes the
+        /// scan points used during tracking
+        /// </summary>
+        protected virtual void InitializeScanPoints()
+        {
+            double angleStep = SweepAngle / NAngles;
+            lock (_scanPointLock)
+            {
+                _pointsToScan = new TailTestCoordinate[NAngles, _nSegments];
+                double segmentDistance = 0;
+                //Compute distance between consecutive segments
+                if (TailIsVertical())
+                {
+                    segmentDistance = Math.Abs(_tailEnd.y - _tailStart.y) / (double)_nSegments;
+                }
+                else
+                    segmentDistance = Math.Abs(_tailEnd.x - _tailStart.x) / (double)_nSegments;
+                //establish scan points:
+                //loop over segments
+                for (int i = 0; i < _nSegments; i++)
+                {
+                    double radius = (i + 1) * segmentDistance;//the radius of the current segment
+                    int lastX = -1;//the last x-coordinate we added to the list
+                    int lastY = -1;//the last y-coordinate we added to the list
+                    int lastIndex = -1;//the last index were we added a point
+                    //loop over all angles - THE FOLLOWING CODE CURRENTLY ASSUMES THE TAIL IS VERTICAL, FACING DOWN!!!
+                    for (int j = 0; j < NAngles; j++)
+                    {
+                        double currAngle = ScanStartAngle + angleStep * j;
+                        int currX = (int)Math.Floor(radius * Math.Sin(currAngle) + TailStart.x);
+                        int currY = (int)Math.Floor(radius * Math.Cos(currAngle) + TailStart.y);
+                        if (currX != lastX || currY != lastY)//new point on circle identified
+                        {
+                            lastIndex++;
+                            _pointsToScan[lastIndex, i] = new TailTestCoordinate(currAngle, radius, new IppiPoint(currX, currY));
+                            lastX = currX;
+                            lastY = currY;
+                        }
+                    }
+                }
+            }//release lock
+        }
+
+        /// <summary>
         /// Uses tail start and end positions as well
         /// as the image size to define the track region sizes
         /// </summary>
@@ -364,7 +473,10 @@ namespace MHApi.Tracking
                 width = _imageSize.width - startx + 1;
             if (starty + height > _imageSize.height)
                 height = _imageSize.height - starty + 1;
-            //compute and trim outer coordinates
+            //compute and trim outer coordinates - morphological operations need to leave
+            //border pixels and don't compute anything useful within the border for those
+            //border pixels - therefore we leave a border twice the size of our structuring
+            //element
             int outerx = startx - 2 * _strel.Anchor.x;
             int outery = starty - 2* _strel.Anchor.y;
             int outerw = width + 2 * _strel.Mask.Width;
@@ -402,6 +514,7 @@ namespace MHApi.Tracking
         /// <returns>NSegments number of TailPoints</returns>
         public TailPoint[] TrackTail(Image8 image)
         {
+            //CURRENTLY ONLY DOWNWARD FACING TAILS ARE PROPERLY SUPPORTED!!!
             //Generate background by closing operation - once a second
             if(_frameNumber % _frameRate == 0)
                 BWImageProcessor.Close(image, _background, _calc1, _strel, _trackRegionOuter);
@@ -409,9 +522,64 @@ namespace MHApi.Tracking
             IppHelper.IppCheckCall(cv.ippiAbsDiff_8u_C1R(_background[_trackRegionInner.TopLeft], _background.Stride, image[_trackRegionInner.TopLeft], image.Stride, _foreground[_trackRegionInner.TopLeft], _foreground.Stride, _trackRegionInner.Size));
             //Threshold
             BWImageProcessor.Im2Bw(_foreground, _thresholded, _trackRegionInner, _threshold);
-
+            
+            //Tracking concept: Whenever new tail coordinates or segment numbers are defined
+            //we compute for each segment in 0.2 degree steps all points on a -90 to +90 degree arc
+            //and store the coordinates with the respective angles and radii
+            //Depending on the actual distance we won't need all 900 points obviously so only
+            //unique coordinates get stored with the rest of the array empty (array will be 900xnSections in size)
+            //During tracking, for each segment we loop over the precomputed coordinates (until the first "empty" coordinate is reached)
+            //and store all pixels where the foreground is white. We do so starting from -90 to +90. Then we remove the first and last
+            //positive coordinate and compute the mean of the remainder
+            //(i.e. we store the sum, the number of points,
+            //the first positive coordinate (<=> min) and the last positive coordinate (<=> max), then subtract max and min from the sum
+            //and divide the sum by (npoints-2))
+            var retval = new TailPoint[_nSegments];
+            lock (_scanPointLock)
+            {
+                //loop over tail segments
+                for (int i = 0; i < _nSegments; i++)
+                {
+                    //loop over all scan-points, exiting loop if empty
+                    //points are reached then find tail angle of this segment
+                    int pointsFound = 0;//the number of non-zero pixels identified
+                    double angleSum = 0;//the sum of all positive angular positions
+                    double minAngle = -100;//the minimal angle that was positive
+                    double maxAngle = -100;//the maximal angle that was positive
+                    for (int j = 0; j < NAngles; j++)
+                    {
+                        if (_pointsToScan[j, i].Radius == 0)//have gone past the end of valid points
+                            break;
+                        //if the value at the current point >0 we take that angle - the first point also updates min angle and
+                        //each point found updates max-angle
+                        if (*_thresholded[_pointsToScan[j, i].Coordinate] > 0)
+                        {
+                            var a = _pointsToScan[j,i].Angle;
+                            if (minAngle == -100)
+                                minAngle = a;
+                            maxAngle = a;
+                            angleSum += a;
+                            pointsFound++;
+                        }
+                    }
+                    //if we have more than two points, compute average after
+                    //removeing the minimum and maximum
+                    double tailAngle;
+                    if (pointsFound == 0)
+                        tailAngle = double.NaN;
+                    else if (pointsFound < 3)
+                        tailAngle = angleSum / pointsFound;
+                    else
+                    {
+                        angleSum -= (minAngle + maxAngle);
+                        pointsFound -= 2;
+                        tailAngle = angleSum / pointsFound;
+                    }
+                    retval[i] = new TailPoint(tailAngle, _pointsToScan[0, i].Radius);
+                }
+            }
             _frameNumber++;
-            return null;
+            return retval;
         }
 
 
