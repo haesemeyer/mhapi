e6cbd4dccfa464d5776852f54152268acf0c2917
Martin Haesemeyer <m.haesemeyer@gmail.com>
2014-04-09 15:43:55 -0400

    Wrote skeleton for class to represent a PG Gazelle camera as a specialization of the CameraLinkCamera class.

---
+++ CameraLink/PGGazelleCamera.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MHApi.CameraLink
+{
+    public unsafe class PGGazelleCamera : CameraLinkCamera
+    {
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructs a new point grey gazelle camera link object
+        /// </summary>
+        /// <param name="interfaceId">The interface name. Optionally followed by ::# identifying the port - 0 based</param>
+        PGGazelleCamera(string interfaceId) : base(interfaceId)
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+        #endregion
+
+        #region Cleanup
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        #endregion
+    }
+}
