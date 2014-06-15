bf71102f29e66b66a60b715074cf617afe1e2621
Martin Haesemeyer <m.haesemeyer@gmail.com>
2014-06-15 15:06:53 -0400

    Added classes with more common use from LaserTracker to MHApi. One class for a list shuffling extension method and one class giving access to a thread safe version of the Random class.

---
+++ Extensions/ListShuffleExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MHApi.RND;
+
+namespace MHApi.Extensions
+{
+    public static class ListShuffleExtension
+    {
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = ThreadSafeRandom.ThisThreadsRandom.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
---
+++ RND/ThreadSafeRandom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading;
+
+namespace MHApi.RND
+{
+    //The following is from a stack-overflow discussion at:
+    //http://stackoverflow.com/questions/273313/randomize-a-listt-in-c-sharp
+
+    public static class ThreadSafeRandom
+    {
+        [ThreadStatic]
+        private static Random Local;
+
+        public static Random ThisThreadsRandom
+        {
+            get { return Local ?? (Local = new Random(unchecked(Environment.TickCount * 31 + Thread.CurrentThread.ManagedThreadId))); }
+        }
+    }
+}
