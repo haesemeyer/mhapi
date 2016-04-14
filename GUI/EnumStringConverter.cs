137b16c01e772da5a501820d44a91a7853924770
Martin Haesemeyer <m.haesemeyer@gmail.com>
2016-04-14 11:07:11 -0400

    Added an enum-string converter to be able to bind enumerations to combo boxes etc.

---
+++ GUI/EnumStringConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace MHApi.GUI
+{
+    public class EnumStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Enum.Parse(targetType, value.ToString(), true);
+        }
+    }
+}
